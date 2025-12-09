95abbea4c9856f33bd276f3d312945d0c37aac4a
Mykyta_zn <mykytazasorin@gmail.com>
2025-12-09 21:02:07 +0200

    auth user upd

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -12,7 +12,11 @@ router.post("/register", async (req, res) => {
   const normalizedEmail = email.toLowerCase();
   try {
     const hashed = await bcrypt.hash(password, 10);
-    const user = new User({ email: normalizedEmail, password: hashed });
+    const user = new User({
+      email: normalizedEmail,
+      password: hashed,
+      role: "user",
+    });
     await user.save();
     res.status(201).json({ message: "User registered" });
   } catch (err) {
