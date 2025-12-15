ea953694d5e53ec98b89ef9d8b3d786ad67187e1
Mykyta_zn <mykytazasorin@gmail.com>
2025-12-15 22:05:47 +0200

    fix names

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import User from "../models/User";
+import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import crypto from "crypto";
